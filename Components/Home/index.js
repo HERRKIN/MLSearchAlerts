faa65ce0a4405108b798d24ed321b0eb12e2ec69
José Andradez <xjgamx@gmail.com>
2017-02-18 17:37:41 -0300

    Cleaning styles

--- Components/Home/index.js
+++ Components/Home/index.js
@@ -16,35 +16,29 @@ export default class Home extends Component {
     super(props)
     this.state={text:''}
   }
+  onSearch(e){
+
+  }
   render() {
     return (
     <Container>
       <Content contentContainerStyle={styles.container}>
 
-      <View style={{minHeight: 200, alignSelf: 'stretch', alignItems: 'center', padding: 10, justifyContent: 'space-between', /*backgroundColor:'red'*/}} >
+      <View style={{minHeight: 210, alignSelf: 'stretch', alignItems: 'center', padding: 10, justifyContent: 'space-between'/*, backgroundColor:'red'*/}} >
         <View style={{alignItems:'center'}}>
           <Icon ios='md-search' android='md-search' style={{color: 'black', fontSize: 80}} />
           <Text style={{fontSize: 20}}>ML Search Alerts</Text>
 
         </View>
-        {/* <Image source={{uri: 'https://openclipart.org/image/2400px/svg_to_png/185356/1382519138.png'}} style={{width: 70, height: 70, marginBottom: 10}} /> */}
-        <TextInput
-          placeholder='Buscar'
-          style={{height: 40, alignSelf: 'stretch', textAlign:'center'}}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
+        <Item>
+            <Input placeholder='Busca lo que quieras' value={this.state.text} style={{ textAlign:'center'}} onChangeText={(text) => this.setState({text})} />
+        </Item>
         <Button
-        //onPress={onPressLearnMore}
+        onPress={this.onSearch}
         title="Buscar"
         color="#300099"
       />
       </View>
-        {/* <Form>
-          <Item>
-            <Input placeholder='Username' />
-          </Item>
-        </Form> */}
     </Content>
     </Container>
     );
@@ -57,15 +51,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'yellow',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  }
+})
